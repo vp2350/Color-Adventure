ea78907781c5e4a73000ec93f6b13afb0ae69ff0
Steven Cho <sxc6061@g.rit.edu>
2018-10-15 17:47:35 -0400

    added tile and player classes

---
+++ Milestone 2 Project/Milestone 2 Project/Player.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Milestone_2_Project
+{
+    class Player
+    {
+        //this class will hold everything partaining to the player position and implimentation
+
+    }
+}
---
+++ Milestone 2 Project/Milestone 2 Project/Tile.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Milestone_2_Project
+{
+    class Tile
+    {
+        //class will have info for each tile (could create seperate classes for seperate tiles)
+
+    }
+}
