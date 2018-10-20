e1dbf458c568b793bff0cb637373e9a6e15aab32
Cullen Sullivan <ces9960@g.rit.edu>
2018-10-19 20:07:28 -0400

    added TileType enum, property, Step() to tile.cs

--- Milestone 2 Project/Milestone 2 Project/Tile.cs
+++ Milestone 2 Project/Milestone 2 Project/Tile.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,5 +13,43 @@ namespace Milestone_2_Project
     {
         //class will have info for each tile (could create seperate classes for seperate tiles)
 
+        public enum TileType { Normal,Death,Buff,NeedBuff,Move};
+
+        TileType type;
+        /*
+        Normal: Safe to walk on
+        Death: You die
+        Buff: Gives you a buff
+        NeedBuff: You die unless you have the buff
+        Move: Moves you to another space
+        */
+        public TileType TType//property for if other classes need to see or change tile type (such as external tool)
+        {
+            get
+            {
+                return type;
+            }
+            set
+            {
+                type = value;
+            }
+        }
+
+        public void Step()//Method to determine what happens if you step on a tile
+        {
+            switch (type)
+            {
+                case TileType.Normal:
+                    break;
+                case TileType.Death:
+                    break;
+                case TileType.Buff:
+                    break;
+                case TileType.NeedBuff:
+                    break;
+                case TileType.Move:
+                    break;
+            }
+        }
     }
 }
