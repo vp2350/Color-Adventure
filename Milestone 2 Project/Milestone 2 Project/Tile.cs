803b6accb303558468e601252abded2fb2fb9751
Steven Cho <sxc6061@g.rit.edu>
2018-10-21 13:03:04 -0700

    added sprites and tile code

--- Milestone 2 Project/Milestone 2 Project/Tile.cs
+++ Milestone 2 Project/Milestone 2 Project/Tile.cs
@@ -13,12 +13,40 @@ namespace Milestone_2_Project
     {
         //class will have info for each tile (could create seperate classes for seperate tiles)
         private Rectangle pos;
+        public enum TileType { Normal, Death, Buff, NeedBuff, Move };
+        Texture2D texture2D;
+        TileType type;
+
+        public TileType Type//property for if other classes need to see or change tile type (such as external tool)
+        {
+            get
+            {
+                return type;
+            }
+            set
+            {
+                type = value;
+            }
+        }
+        public Texture2D Texture2D
+        {
+            get
+            {
+                return texture2D;
+            }
+            set
+            {
+                texture2D = value;
+            }
+        }
+
         public Tile(int x, int y, int width, int height)             //Main constructor
             :base(x,y,width,height)
         {
             position = new Rectangle(x, y, width, height);
         }
-            public bool CheckCollision(GameObject objToCheck)
+
+        public bool CheckCollision(GameObject objToCheck)
         {
             //check collision between this object and any given object
             if (pos.Intersects(objToCheck.Position))
@@ -28,6 +56,31 @@ namespace Milestone_2_Project
             else
             {
                 return false;
+            }
+        }
+        /*
+        Normal: Safe to walk on
+        Death: You die
+        Buff: Gives you a buff
+        NeedBuff: You die unless you have the buff
+        Move: Moves you to another space
+        */
+
+
+        public void Step()//Method to determine what happens if you step on a tile
+        {
+            switch (type)
+            {
+                case TileType.Normal:
+                    break;
+                case TileType.Death:
+                    break;
+                case TileType.Buff:
+                    break;
+                case TileType.NeedBuff:
+                    break;
+                case TileType.Move:
+                    break;
             }
         }
     }
